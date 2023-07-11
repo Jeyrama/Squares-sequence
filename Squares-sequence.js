afc7d8fcf38a8946f54149992e14b6e1b30dfe6b
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-10 21:09:17 -0700

    Update Squares-sequence.js

--- Squares-sequence.js
+++ Squares-sequence.js
@@ -19,3 +19,12 @@ function squares(x, n) {
 }
 
 // or
+
+function squares(x, n) {
+  let arr = [];
+  if(n === 0 || n < 0) return arr;
+  for(let i = x; arr.length < n; i*=i) {
+      arr.push(i);
+  }
+  return arr
+}
